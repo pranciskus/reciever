481e0d1bfe7fce1b6198c43d05800ce4fc174ece
cmllr <fury224@googlemail.com>
2021-01-16 11:24:17 +0100

    Added reciever.bat to bootstrap pip and requirements

---
+++ reciever.bat
@@ -0,0 +1,15 @@
+@echo off
+python.exe --version
+
+set needle = Lib
+
+if not exist "Lib" (
+  REM Download pip
+  powershell -Command "Invoke-WebRequest https://bootstrap.pypa.io/get-pip.py -OutFile get-pip.py"
+  REM install pip
+  python.exe get-pip.py
+  REM insta install dependencies
+  python.exe -m pip install -r requirements.txt
+) else (
+  python.exe reciever.py
+)
