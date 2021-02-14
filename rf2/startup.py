be299a53e4c24e8bb84fc5ca63e291744fb48ddc
cmllr <fury224@googlemail.com>
2021-02-14 13:50:40 +0100

    Fix #9

--- rf2/startup.py
+++ rf2/startup.py
@@ -3,32 +3,41 @@ from os import remove
 from time import sleep
 
 from pywinauto import Application, WindowSpecification
-from rf2.util import set_slider_value, set_updown_value, select_from_list, set_window_elements_value
+from rf2.util import (
+    set_slider_value,
+    set_updown_value,
+    select_from_list,
+    set_window_elements_value,
+    get_server_port,
+    get_max_players,
+)
 from rf2.interaction import chat, do_action, Action
+import logging
 
 
 def oneclick_start_server(server_config: dict) -> bool:
     root_path = server_config["server"]["root_path"]
     server_root_path = join(server_config["server"]["root_path"], "server")
     mod = server_config["mod"]["mod"]
-    server_binary_path = join(
-        server_root_path, "Bin64", "rFactor2 Dedicated.exe")
-    server_binary_commandline = server_binary_path + \
-        f" +path=\"{server_root_path}\"" + \
-        f"  +profile=player  +oneclick"
+    server_binary_path = join(server_root_path, "Bin64", "rFactor2 Dedicated.exe")
+    server_binary_commandline = (
+        server_binary_path
+        + f' +path="{server_root_path}"'
+        + f"  +profile=player  +oneclick"
+    )
 
     # make sure the Dedicated<modname>.ini has the correct content
     mod_ini_path = join(
-        server_root_path, "UserData", "player", "Dedicated" + mod["name"] + ".ini")
+        server_root_path, "UserData", "player", "Dedicated" + mod["name"] + ".ini"
+    )
 
-    max_clients_overwrite = str(server_config["mod"]["server"]["overwrites"]
-                                ["Multiplayer.JSON"]["Multiplayer Server Options"]["Max MP Players"])
+    max_clients_overwrite = get_max_players(server_config)
 
     if exists(mod_ini_path):
         remove(mod_ini_path)
     with open(mod_ini_path, "w") as file:
         file.write("[SETTINGS]\n")
-        file.write("MaxClients=" + max_clients_overwrite + "\n")
+        file.write("MaxClients=" + str(max_clients_overwrite) + "\n")
         file.write("[TRACKS]\n")
     rf2_server = Application().start(server_binary_commandline)
     return True
@@ -48,15 +57,16 @@ def stop_server(server_config: dict) -> bool:
 
     do_action(server_config, Action.RESTARTWEEKEND)
     root_path = join(server_config["server"]["root_path"], "server")
-    binary_path = join(root_path, "Bin64",
-                       "rFactor2 Dedicated.exe")
+    binary_path = join(root_path, "Bin64", "rFactor2 Dedicated.exe")
     app = Application(backend="uia")
     app.connect(path=binary_path)
     app.kill()
     return True
 
 
-def select_mod_and_admin_password(window: WindowSpecification, admin_password: str, mod_name: str, mod_version: str):
+def select_mod_and_admin_password(
+    window: WindowSpecification, admin_password: str, mod_name: str, mod_version: str
+):
     """
     Select the mod and inserts the admin password
 
@@ -67,14 +77,21 @@ def select_mod_and_admin_password(window: WindowSpecification, admin_password: s
         mod_version: The mod version: Important: WITHOUT a "v" prefix!
     """
     mod_Box = window.window(
-        best_match="Select which game database to use for this server:ComboBox")
+        best_match="Select which game database to use for this server:ComboBox"
+    )
     mods = mod_Box.item_texts()
     mod_Box.select(mods.index(f"{mod_name} v{mod_version}"))
 
     window.window(best_match="Edit").set_text(admin_password)
 
 
-def select_grip(start_window: WindowSpecification, grips: dict, tracks: dict, server_root_path: str, rf2_server: Application):
+def select_grip(
+    start_window: WindowSpecification,
+    grips: dict,
+    tracks: dict,
+    server_root_path: str,
+    rf2_server: Application,
+):
     """
     Select the mod and inserts the admin password
 
@@ -89,21 +106,21 @@ def select_grip(start_window: WindowSpecification, grips: dict, tracks: dict, se
     # select the track
     for _, track in tracks.items():
         layout = track["layout"]
-        event_list = start_window.window(
-            best_match='Selected Events:ListBox')
+        event_list = start_window.window(best_match="Selected Events:ListBox")
         select_from_list(event_list, [layout])
         sleep(1)
         # open weather and grip window
-        start_window.window(best_match='Weather').click()
+        start_window.window(best_match="Weather").click()
         grip_window = rf2_server.window(best_match=server_root_path)
-        grip_window.wait('visible')
+        grip_window.wait("visible")
         grip_window.minimize()
 
         # Select the grip itself
-        session_select = grip_window.window(best_match='ComboBox11')
-        grip_mode_select = grip_window.window(best_match='Real Road:ListBox')
+        session_select = grip_window.window(best_match="ComboBox11")
+        grip_mode_select = grip_window.window(best_match="Real Road:ListBox")
         grip_scale_select = grip_window.window(
-            best_match='Real Road Time Scale:ComboBox13')
+            best_match="Real Road Time Scale:ComboBox13"
+        )
         for session, grip in grips.items():
             grip_mode = grip["grip_mode"]
             grip_scale = grip["grip_scale"]
@@ -111,4 +128,4 @@ def select_grip(start_window: WindowSpecification, grips: dict, tracks: dict, se
             select_from_list(grip_mode_select, [grip_mode])
             select_from_list(grip_scale_select, [grip_scale])
 
-        grip_window.window(best_match='Save').click()
+        grip_window.window(best_match="Save").click()
