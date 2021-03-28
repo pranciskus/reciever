105d0168e1fbdb023f4f22e7e54cfbda905662ec
cmllr <fury224@googlemail.com>
2021-03-28 18:26:46 +0200

    Add nearby and team parameter to low speed hook

--- rf2/events/__init__.py
+++ rf2/events/__init__.py
@@ -1,5 +1,7 @@
 LOW_SPEED_THRESHOLD = 50
 
+STATUS_INTERVALS_FOR_LOW_SPEED_HOOK = 5
+
 
 def get_prop_map(drivers, property):
     result = {}
--- rf2/events/onLowSpeed.py
+++ rf2/events/onLowSpeed.py
@@ -1,5 +1,5 @@
 from math import sqrt
-from rf2.events import LOW_SPEED_THRESHOLD
+from rf2.events import LOW_SPEED_THRESHOLD, STATUS_INTERVALS_FOR_LOW_SPEED_HOOK
 
 
 def get_speed(drivers):
@@ -13,9 +13,30 @@ def get_speed(drivers):
             result[driver["driverName"]] = {
                 "speed": speed,
                 "lapDistance": driver["lapDistance"],
+                "nearby": [],
+                "teamName": driver["vehicleName"],
             }
 
+    for driver_name, infos in result.items():
+        # find nearby drivers
+        if infos["speed"] < LOW_SPEED_THRESHOLD:
+            location = infos["lapDistance"]
+            maximum = location + 100
+            minimum = location - 200
+            to_be_warned = []
+            for other_driver, other_driver_infos in result.items():
+                other_driver_location = other_driver_infos["lapDistance"]
+                if (
+                    other_driver_location > minimum
+                    and other_driver_location < maximum
+                    and driver_name != other_driver
+                ):
+                    to_be_warned.append(other_driver)
+            infos["nearby"] = to_be_warned
     return result
+
+
+warns = {}
 
 
 def onLowSpeed(oldStatus, newStatus, all_hooks):
@@ -26,5 +47,16 @@ def onLowSpeed(oldStatus, newStatus, all_hooks):
 
         for driver, speed in new_driver_speed.items():
             if speed["speed"] < LOW_SPEED_THRESHOLD:
-                for hook in all_hooks:
-                    hook(driver, speed["speed"], speed["lapDistance"])
+                if driver not in warns:
+                    warns[driver] = 0
+                warns[driver] = warns[driver] + 1
+                if warns[driver] > STATUS_INTERVALS_FOR_LOW_SPEED_HOOK:
+                    for hook in all_hooks:
+                        hook(
+                            driver,
+                            speed["speed"],
+                            speed["lapDistance"],
+                            speed["nearby"],
+                            speed["teamName"],
+                        )
+                    del warns[driver]
