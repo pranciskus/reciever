e6d6bdfd2f7773e506a693ae382a6e90fe3abc1b
cmllr <fury224@googlemail.com>
2021-07-24 11:31:57 +0200

    Removed pywinauto for startup

--- rf2/startup.py
+++ rf2/startup.py
@@ -1,8 +1,6 @@
 from os.path import join, exists
 from os import remove
 from time import sleep
-
-from pywinauto import Application, WindowSpecification
 from rf2.util import (
     set_slider_value,
     set_updown_value,
@@ -17,6 +15,7 @@ import logging
 from subprocess import Popen, STARTUPINFO, HIGH_PRIORITY_CLASS
 from string import ascii_uppercase, digits
 from random import choice
+from psutil import process_iter
 
 
 def oneclick_start_server(server_config: dict) -> bool:
@@ -67,79 +66,18 @@ def stop_server(server_config: dict) -> bool:
     Returns:
         The success of the operation
     """
-    chat(server_config, "Server is shutting down NOW")
 
-    do_action(server_config, Action.RESTARTWEEKEND)
     root_path = join(server_config["server"]["root_path"], "server")
     binary_path = join(root_path, "Bin64", "rFactor2 Dedicated.exe")
-    app = Application(backend="uia")
-    app.connect(path=binary_path)
-    app.kill()
-    return True
+    chat(server_config, "Server is shutting down NOW")
+    do_action(server_config, Action.RESTARTWEEKEND)
+    sleep(5)  # Give the server a chance to react
 
-
-def select_mod_and_admin_password(
-    window: WindowSpecification, admin_password: str, mod_name: str, mod_version: str
-):
-    """
-    Select the mod and inserts the admin password
-
-    Args:
-        window: WindowSpecification
-        admin_password: The admin password string
-        mod_name: The mod name string
-        mod_version: The mod version: Important: WITHOUT a "v" prefix!
-    """
-    mod_Box = window.window(
-        best_match="Select which game database to use for this server:ComboBox"
-    )
-    mods = mod_Box.item_texts()
-    mod_Box.select(mods.index(f"{mod_name} v{mod_version}"))
-
-    window.window(best_match="Edit").set_text(admin_password)
-
-
-def select_grip(
-    start_window: WindowSpecification,
-    grips: dict,
-    tracks: dict,
-    server_root_path: str,
-    rf2_server: Application,
-):
-    """
-    Select the mod and inserts the admin password
-
-    Args:
-        start_window: WindowSpecification
-        grips: A dictionary containing the session keyed grip infos
-        tracks: The workshop_id keyed track infos
-        server_root_path: The root path of the server
-        rf2_server: The Application instance of the rf2 server
-    """
-
-    # select the track
-    for _, track in tracks.items():
-        layout = track["layout"]
-        event_list = start_window.window(best_match="Selected Events:ListBox")
-        select_from_list(event_list, [layout])
-        sleep(1)
-        # open weather and grip window
-        start_window.window(best_match="Weather").click()
-        grip_window = rf2_server.window(best_match=server_root_path)
-        grip_window.wait("visible")
-        grip_window.minimize()
-
-        # Select the grip itself
-        session_select = grip_window.window(best_match="ComboBox11")
-        grip_mode_select = grip_window.window(best_match="Real Road:ListBox")
-        grip_scale_select = grip_window.window(
-            best_match="Real Road Time Scale:ComboBox13"
-        )
-        for session, grip in grips.items():
-            grip_mode = grip["grip_mode"]
-            grip_scale = grip["grip_scale"]
-            select_from_list(session_select, [session])
-            select_from_list(grip_mode_select, [grip_mode])
-            select_from_list(grip_scale_select, [grip_scale])
-
-        grip_window.window(best_match="Save").click()
+    for proc in process_iter():
+        name = proc.name()
+        if "rFactor" in name:
+            exe = proc.exe()
+            if exe == binary_path:
+                proc.kill()
+                return True
+    return False
