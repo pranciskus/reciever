0e3a6c3da7005de04502c34cdf60157a29107cc2
cmllr <fury224@googlemail.com>
2021-07-31 13:49:38 +0200

    Add pip on build

--- reciever.bat
+++ reciever.bat
@@ -4,8 +4,6 @@
 if not exist "server.json" (
   REM add demo mod
   copy mod.json.tpl mod.json
-  REM Download pip
-  powershell -Command "Invoke-WebRequest https://bootstrap.pypa.io/get-pip.py -OutFile ..\get-pip.py"
   REM install pip
   ..\python.exe ..\get-pip.py
   REM insta install dependencies
