7605ff85e10d6858e47782929a0b6be1afd82c54
cmllr <fury224@googlemail.com>
2021-08-28 17:43:11 +0200

    Refined log message

--- rf2/deploy.py
+++ rf2/deploy.py
@@ -402,7 +402,7 @@ def get_latest_version(root_path: str, latest=True) -> str:
     versions = listdir(root_path)
     if len(versions) == 1:
         logging.info(
-            "We don't need to sort here as only one version ({}) is available. Falling back.".format(
+            "We don't need to sort here as only one version ({}) for {} is available. Falling back.".format(
                 versions[0], root_path
             )
         )
